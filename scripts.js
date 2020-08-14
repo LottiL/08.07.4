91e47261e69cf4636900eab6743d5bac43a3a151
LottiL <69212700+LottiL@users.noreply.github.com>
2020-08-14 12:02:00 +0200

    next task

--- scripts.js
+++ scripts.js
@@ -1,11 +1,17 @@
 $(".first").on('click', () => {
     console.log("Yeah, you clicked me")
-  });
+});
 
-  $(".second").on('click', () => {
+$(".second").on('click', () => {
     $( ".first" ).text("Button")
-  });
+});
 
-  $(".third").on('click', () => {
+ /* $(".third").on('click', () => {
     $("button").css("background-color", "yellow");
-  });  
+  });  */
+
+$(".third").on('click', () => {
+    let color = $("input").val();
+    console.log(color);
+    $("button").css("background-color", color);
+}); 
